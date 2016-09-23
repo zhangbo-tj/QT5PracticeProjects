b95b55a2b0248ef37b0c02aa5638f39874a28f26
BigHippo <bozhang_sy@126.com>
2016-09-24 02:45:50 +0800

    Add files via upload

---
+++ DialogExample/baseinfo.h
@@ -0,0 +1,49 @@
+﻿#ifndef BASEINFO_H
+#define BASEINFO_H
+
+#include <QWidget>
+
+class QLabel;
+class QLineEdit;
+class QComboBox;
+class QTextEdit;
+class QPushButton;
+
+class QVBoxLayout;
+class QHBoxLayout;
+class QGridLayout;
+
+class BaseInfo : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit BaseInfo(QWidget *parent = 0);
+
+private:
+
+    //左侧
+    QLabel *usernameLabel;
+    QLabel *nameLabel;
+    QLabel *sexLabel;
+    QLabel *departmentLabel;
+    QLabel *ageLabel;
+    QLabel *otherLabel;
+    QLineEdit *usernameLineEdit;
+    QLineEdit *nameLineEdit;
+    QComboBox *sexComboBox;
+    QTextEdit *departmentTextEdit;
+    QLineEdit *ageLineEdit;
+    QGridLayout *leftLayout;
+
+    //右侧
+    QLabel *headLabel;
+    QLabel *headIconLabel;
+    QPushButton *updateButton;
+    QHBoxLayout *topRightLayout;
+
+    QLabel *introLabel;
+    QTextEdit *introTextEdit;
+    QVBoxLayout *rightLayout;
+};
+
+#endif // BASEINFO_H
---
+++ DialogExample/contact.cpp
@@ -0,0 +1,49 @@
+﻿#include "contact.h"
+
+#include <QLabel>
+#include <QLineEdit>
+#include <QGridLayout>
+#include <QCheckBox>
+
+Contact::Contact(QWidget *parent) : QWidget(parent)
+{
+    emailLabel = new QLabel(QString::fromLocal8Bit("电子邮件:"));
+    emailLineEdit = new QLineEdit;
+
+    addressLabel = new QLabel(QString::fromLocal8Bit("联系地址："));
+    addressLineEdit = new QLineEdit;
+
+    codeLabel = new QLabel(QString::fromLocal8Bit("邮政编码："));
+    codeLineEdit = new QLineEdit;
+
+    cellphoneLabel = new QLabel(QString::fromLocal8Bit("移动电话"));
+    cellphoneLineEdit = new QLineEdit;
+    acceptMessBox = new QCheckBox(QString::fromLocal8Bit("接收留言"));
+    acceptMessBox->setCheckState(Qt::Unchecked);
+
+    phoneLabel = new QLabel(QString::fromLocal8Bit("办公电话："));
+    phoneLineEdit = new QLineEdit;
+
+    mainLayout = new QGridLayout(this);
+    mainLayout->setMargin(15);
+    mainLayout->setSpacing(10);
+
+    mainLayout->addWidget(emailLabel,0,0);
+    mainLayout->addWidget(emailLineEdit,0,1);
+
+    mainLayout->addWidget(addressLabel,1,0);
+    mainLayout->addWidget(addressLineEdit,1,1);
+
+    mainLayout->addWidget(codeLabel,2,0);
+    mainLayout->addWidget(codeLineEdit,2,1);
+
+    mainLayout->addWidget(cellphoneLabel,3,0);
+    mainLayout->addWidget(cellphoneLineEdit,3,1);
+    mainLayout->addWidget(acceptMessBox,3,2);
+
+    mainLayout->addWidget(phoneLabel,4,0);
+    mainLayout->addWidget(phoneLineEdit,4,1);
+
+    mainLayout->setSizeConstraint(QLayout::SetFixedSize);
+
+}
